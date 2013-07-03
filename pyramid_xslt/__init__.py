ed1bd98921559d4895a87ccb3a196147912f6d9b
Cyprien Le Pannérer <cyplp@free.fr>
2013-07-03 18:33:43 +0200

    Using pyramid's registry instead zope's gsm

--- pyramid_xslt/__init__.py
+++ pyramid_xslt/__init__.py
@@ -2,17 +2,13 @@
 import os.path
 
 from zope.interface import implementer
-from zope.component import getGlobalSiteManager
-from zope.component import getUtility
 from zope.component import ComponentLookupError
 
 from lxml import etree
 
 from pyramid.interfaces import IRenderer
 from pyramid.path import package_path
-from pyramid.response import Response
-
-gsm = getGlobalSiteManager()
+#from pyramid.response import Response
 
 
 def includeme(config):
@@ -35,12 +31,13 @@ class XsltRendererFactory(object):
 
         The renderer is cached for optimize access.
         """
+        registry = system['request'].registry
         try:
-            xsl = getUtility(IRenderer, system['renderer_name'])
+            xsl = registry.getUtility(IRenderer, system['renderer_name'])
         except ComponentLookupError:
             xsl = XslRenderer(os.path.join(package_path(self._info.package),
                                            system['renderer_name']))
-            gsm.registerUtility(xsl, IRenderer, system['renderer_name'])
+            registry.registerUtility(xsl, IRenderer, system['renderer_name'])
         return xsl(value, system)
 
 
